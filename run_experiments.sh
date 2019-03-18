4e39c67b2a516c1f50496195e1814889ef75c1d4
Quan Vuong <quan.vuong@nyu.edu>
2019-03-17 18:20:42 -0700

    correct flag is env not env_name

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,42 +3,42 @@
 for ((i=0;i<5;i+=1))
 do
 	python main.py \
-	--env_name "HalfCheetah-v2" \
+	--env "HalfCheetah-v2" \
 	--seed $i &
 
 	python main.py \
-	--env_name "Hopper-v2" \
+	--env "Hopper-v2" \
 	--seed $i &
 
 	python main.py \
-	--env_name "Walker2d-v2" \
+	--env "Walker2d-v2" \
 	--seed $i &
 
 	python main.py \
-	--env_name "Ant-v2" \
+	--env "Ant-v2" \
 	--seed $i &
 
 	python main.py \
-	--env_name "InvertedPendulum-v2" \
+	--env "InvertedPendulum-v2" \
 	--seed $i &
 
 	python main.py \
-	--env_name "InvertedDoublePendulum-v2" \
+	--env "InvertedDoublePendulum-v2" \
 	--seed $i &
 
 	python main.py \
-	--env_name "Reacher-v2" \
+	--env "Reacher-v2" \
 	--seed $i &
 
 	python main.py \
-	--env_name "Humanoid-v2" \
+	--env "Humanoid-v2" \
 	--seed $i &
 
 	python main.py \
-	--env_name "HumanoidStandup-v2" \
+	--env "HumanoidStandup-v2" \
 	--seed $i &
 
 	python main.py \
-	--env_name "Swimmer-v2" \
+	--env "Swimmer-v2" \
 	--seed $i &
 done
